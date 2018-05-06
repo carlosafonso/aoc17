6a90896ff5d4ec62f67b362e1799cac6b969a7b8
Carlos Afonso Pérez <carlos.afonso.perez@gmail.com>
2018-05-06 19:55:25 +0200

    Add test 2b

--- aoc2.py
+++ aoc2.py
@@ -8,3 +8,25 @@ def aoc2_a(input):
         checksum += max_item - min_item
 
     return checksum
+
+
+def aoc2_b(input):
+    rows = input.split("\n")
+    checksum = 0
+    for row in rows:
+        items = list(map(int, row.split("\t")))
+        done = False
+        for k, i in enumerate(items):
+            for j in items[k + 1:]:
+                if i > j and i % j == 0:
+                    checksum += int(i / j)
+                    done = True
+                    break
+                if i < j and j % i == 0:
+                    checksum += int(j / i)
+                    done = True
+                    break
+            if done:
+                break
+
+    return checksum
--- tests/test_aoc2.py
+++ tests/test_aoc2.py
@@ -7,3 +7,10 @@ import aoc2
 ])
 def test_aoc2_a(test_input, expected):
     assert expected == aoc2.aoc2_a(test_input)
+
+
+@pytest.mark.parametrize("test_input,expected", [
+    ("5\t9\t2\t8\n9\t4\t7\t3\n3\t8\t6\t5", 9)
+])
+def test_aoc2_b(test_input, expected):
+    assert expected == aoc2.aoc2_b(test_input)
